5d07c665bec765f913a9f632b79cb8fac8155566
n <nelson+github@dwyl.com>
2025-08-10 23:08:57 +0100

    anyCommonElements generic function

--- swift/hello.swift
+++ swift/hello.swift
@@ -656,3 +656,24 @@ func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
 }
 let arr = makeArray(repeating: "knock", numberOfTimes: 4)
 print("array: \(arr)")
+
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+print("anyCommonElements: \(anyCommonElements([1, 2, 3], [3]))")
