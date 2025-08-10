9646141168ef2efba6b347d066f9098c895f42cc
n <nelson+github@dwyl.com>
2025-08-10 22:48:54 +0100

    generic array

--- swift/hello.swift
+++ swift/hello.swift
@@ -643,3 +643,16 @@ if fridgeContains("banana") {
 }
 print("fridgeIsOpen: \(fridgeIsOpen)")
 // Prints "false"
+
+// Gnerics
+// Write a name inside angle brackets to make a generic function or type.
+
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result: [Item] = []
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+let arr = makeArray(repeating: "knock", numberOfTimes: 4)
+print("array: \(arr)")
